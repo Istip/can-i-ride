3e2962c873c531f67246f60798d0442a537f507c
Istip <isticsek@gmail.com>
2022-01-05 16:19:38 +0200

    added temperature, city name and weather icon

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState('Odorheiu Secuiesc');
+  const [value, setValue] = useState('');
   const [city, setCity] = useState('Odorheiu Secuiesc');
   const [loading, setLoading] = useState(false);
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState('');
 
   const apiKey = process.env.REACT_APP_API_KEY;
 
   const handleSelectCity = (e) => {
     e.preventDefault();
     setCity(value);
+    setValue('');
   };
 
   useEffect(() => {
@@ -26,18 +27,12 @@ function App() {
         console.log(res.data);
         setLoading(false);
       })
-      // REMOVE this line soon!
-      .then(() => console.log(weather))
       .catch((error) => {
         console.log('API error:', error);
         setLoading(false);
       });
     // eslint-disable-next-line
   }, [city, apiKey]);
-
-  if (loading) {
-    return 'Loading...';
-  }
 
   return (
     <div className="App">
@@ -47,8 +42,7 @@ function App() {
         ) : (
           <>
             <h1>Can I ride my bike?</h1>
-            <br />
-            <h3>Just a weather forecast, but this time for bikers!</h3>
+            <h3>Weather forecast for bikers!</h3>
             <br />
 
             <form onSubmit={handleSelectCity}>
@@ -57,10 +51,30 @@ function App() {
                 type="text"
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
+                placeholder="Enter city name..."
+                required
               />
-              <button type="submit">Check out!</button>
+              <button type="submit" style={{ padding: '16px 32px' }}>
+                Check out!
+              </button>
             </form>
           </>
+        )}
+
+        <br />
+
+        {typeof weather === 'object' && (
+          <div>
+            <h2>
+              {weather.name} <small>({weather.sys.country})</small>
+            </h2>
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt=""
+            />
+            <br />
+            <p>{Math.floor(weather.main.temp - 273.15)} °C</p>
+          </div>
         )}
       </header>
     </div>
